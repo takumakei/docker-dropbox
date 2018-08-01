0c59e8da7d593c2026d3481cac7fc32f85d19e28
takumakei <takumakei@gmail.com>
2018-08-02 00:20:11 +0900

    add check.sh - script of 'check for updates'

---
+++ check.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eu
+
+main() {
+  local mine latest
+  mine="$(cat Dockerfile | extract_version)"
+  latest="$(curl --head -sS 'https://www.dropbox.com/download?plat=lnx.x86_64' | extract_version)"
+
+  if [[ "$mine" = "$latest" ]]; then
+    echo "up to date (version:$latest)"
+  else
+    echo "updated (latest:$latest != $latest:Dockerfile)"
+    exit 1
+  fi
+}
+
+extract_version() {
+  sed -e '/dbx-releng\/client\/dropbox-lnx/!d' -e 's/^.*x86_64-\(.*\)\.tar\.gz.*/\1/'
+}
+
+main "$@"
