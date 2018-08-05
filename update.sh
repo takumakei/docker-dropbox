60c87b169c65a95c7be27a78371f8d01d187dbdb
takumakei <takumakei@gmail.com>
2018-08-05 17:57:00 +0900

    use tini as ENTRYPOINT

--- update.sh
+++ update.sh
@@ -34,6 +34,12 @@ RUN mkdir /dropboxd \\
 
 FROM debian:stretch-slim
 
+# Add Tini
+ENV TINI_VERSION v0.18.0
+ADD https://github.com/krallin/tini/releases/download/\${TINI_VERSION}/tini /tini
+RUN chmod +x /tini
+ENTRYPOINT ["/tini", "--"]
+
 RUN apt-get update \\
  && apt-get install -y --no-install-recommends ca-certificates python \\
  && rm -rf /var/lib/apt/lists/*
